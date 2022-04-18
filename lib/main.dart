5b6afb631dc37d9e8dbea2678f3390c5068e9ce7
SyedKashafi <syedkasha96@gmail.com>
2022-04-18 17:11:23 +0800

    commit to dev

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dppr/JsonDataModel.dart';
 import 'package:flutter/services.dart' as rootBundle;
